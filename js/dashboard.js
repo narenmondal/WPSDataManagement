3e4c6923d77a968d0c49131fa098e2f675230e21
Ashraf Iqbal <merchantai@gmail.com>
2019-08-02 17:20:49 +0530

    commited on 2 Aug 2019

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,31 @@
 $(document).ready(function () {
 
+    $("#add").click(function () {
+        $("#addMenu").toggle(200);
+    });
+
+    $("#addMaterial").click(function () {
+        $("#addMenu").hide(200);
+    });
+
+    $("#addCustomer").click(function () {
+        $("#addMenu").hide(200);
+    });
+    $("#addVendor").click(function () {
+        $("#addMenu").hide(200);
+    });
+
+    $("#addAssets").click(function () {
+        $("#addMenu").hide(200);
+    });
+    $("#addCatalog").click(function () {
+        $("#addMenu").hide(200);
+    });
+
+    $("#addReport").click(function () {
+        $("#addMenu").hide(200);
+    });
+
     $("#openNav").click(function () {
         $("#navbar").css("width", "200px");
     });
